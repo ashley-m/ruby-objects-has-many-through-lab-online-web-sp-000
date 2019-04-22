f39d2af53015467eb792400cd1e545663740fdcc
Ashley Miller <asm2fe@virginia.edu>
2019-04-22 20:03:04 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,12 +1,13 @@
 class Patient
 
-attr_accessor :name, :doctors
+attr_accessor :name, :doctors, :appointments
 
 @@all = []
 
 def initialize(name)
   @name = name
   @doctors = []
+  @appointments = []
   @@all << self
 end
 
@@ -14,5 +15,6 @@ def new_appointment(doctor, date)
   a = Appointment.new(date, self, doctor)
   doctor.patients << self
   @doctors << doctor
+  @appointments << a
 end
 end
